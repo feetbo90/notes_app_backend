8d1ff3471c62e976e1733c111c9787c9b9d0e2d7
Angga Hasibuan <dickyhasibuan@Dickys-MacBook-Pro.local>
2022-12-19 12:25:58 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+// const routes = require('./routes');
+// const notesPlugin = require('./notesPlugin');
+const notes = require('./api/notes');
+const NotesService = require('./services/postgres/NotesService');
+const NotesValidator = require('./validator/notes');
+
+const init = async () => {
+  const notesService = new NotesService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  // const server = Hapi.server();
+
+  // registrasi satu plugin
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+
+  // server.route(routes);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
